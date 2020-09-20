8820ca2b90aadebc68e4859a93daff99d65def1d
Takasakiii <lucasmc2709@live.com>
2020-09-20 20:49:05 -0300

    Correção na exibição da pagina tags

--- routes/tag.js
+++ routes/tag.js
@@ -5,12 +5,13 @@ const { partialBotObject } = require("../utils/bot");
 
 module.exports = (db) => {
     router.get("/:tag", (req, res) => {
-        const tagName = req.params.tag
+        const tagName = req.params.tag;
+        const tagFormated = tags[tagName];
         let page = req.query.page;
         if (!page || isNaN(page) || page < 1) 
             page = 1
         db.Bots.find({"details.tags": tagName}).limit(18).skip((page-1) * 18).exec().then(bots => {
-            res.render("tags", {title: tagName, tag: tagName, page, bots: (bots || []).map(partialBotObject)});
+            res.render("tags", {title: tagFormated, tag: tagFormated, page, bots: (bots || []).map(partialBotObject)});
         });
     });
 
