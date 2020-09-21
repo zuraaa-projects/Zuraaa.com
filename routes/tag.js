5476054f7b8bce4e209cde3ce235f8ab79ea2065
Takasakiii <lucasmc2709@live.com>
2020-09-20 21:06:16 -0300

    navegação por tags ta melhor, n precisando voltar a index

--- routes/tag.js
+++ routes/tag.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const tags = require("../utils/tags");
 const { partialBotObject } = require("../utils/bot");
+const colors = require("../utils/colors");
 
 module.exports = (db) => {
     router.get("/:tag", (req, res) => {
@@ -11,7 +12,14 @@ module.exports = (db) => {
         if (!page || isNaN(page) || page < 1) 
             page = 1
         db.Bots.find({"details.tags": tagName}).limit(18).skip((page-1) * 18).exec().then(bots => {
-            res.render("tags", {title: tagFormated, tag: tagFormated, page, bots: (bots || []).map(partialBotObject)});
+            res.render("tags", {
+                title: tagFormated, 
+                tag: tagFormated, 
+                page, 
+                bots: (bots || []).map(partialBotObject),
+                tags,
+                colors
+            });
         });
     });
 
