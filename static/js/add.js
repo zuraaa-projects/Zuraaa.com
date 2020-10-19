25032fd684588b165dc62d8cf41dc48ad402c2a1
Takasakiii <lucasmc2709@live.com>
2020-10-19 19:38:30 -0300

    remove form validation bug

--- static/js/add.js
+++ static/js/add.js
@@ -18,50 +18,6 @@ $(function() {
         else if (childLen == 4)
             addOwner.toggleClass("is-hidden");
     });
-    
-
-    $("#form").submit(function(event) {
-        var error;
-        var id = $("#id");
-        if (!isId(id.val()))
-            error = id;
-        else {
-            for (owner of $(".owner-input")) {
-                if (!isId(owner.value)) {
-                    error = $(owner);
-                    break;
-                }
-            };
-            if (!error) {
-                var lib = $("#library");
-                if (!lib.val())
-                    error = lib;
-                else {
-                    var prefix = $("#prefix");
-                    if (!prefix.val())
-                        error = prefix;
-                    else {
-                        var shortdesc = $("#shortdesc");
-                        if (!shortdesc.val())
-                            error = shortdesc;
-                        else {
-                            var tags = $("#tags");
-                            var len = tags.val().length;
-                            if (!len || len > 6)
-                                error = tags;
-                        }
-                    }
-                }
-            }
-        }
-        if (error) {
-            error.focus();
-            $("#addtitle").get(0).scrollIntoView();
-            error.addClass("is-danger");
-            setTimeout(function(){error.removeClass("is-danger")}, 2000)
-            event.preventDefault();
-        }
-    });
 });
 
 function isId(st) {
