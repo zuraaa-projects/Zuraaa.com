26c8d1a6590b9beb476a0bae0494215391f7cf38
LuckShiba <43530291+LuckShiba@users.noreply.github.com>
2021-02-17 17:03:42 -0300

    Update discordbot.js

--- utils/discordbot.js
+++ utils/discordbot.js
@@ -10,7 +10,7 @@ module.exports = (config) => {
   async function fetchUser(id) {
     const response = await fetch(`http://localhost:5001/users/${id}`);
     if (response.status === 200) {
-      const bot = response.json();
+      const bot = await response.json();
       bot.discriminator = bot.discriminator.toString().padStart(4, '0')
       return bot
     }
