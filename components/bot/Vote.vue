25b1f569a52ddb11993c50d1cd5c04f1d06fee0b
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-04-13 01:04:14 -0300

    Base do card de votar

---
+++ components/bot/Vote.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="vote">
+    <div class="vote__card">
+      <h1>Vote em {{ bot.username }}</h1>
+      <img src="" alt="" srcset="">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import type { Bot } from '~/models/bots/bot'
+
+@Component
+export default class extends Vue {
+  @Prop({
+    required: true
+  })
+  bot!: Bot
+}
+</script>
+
+<style lang="scss" scoped>
+.vote {
+  width: 280px;
+  &__card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
